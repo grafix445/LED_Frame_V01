db87f3f8243ef6e7a641014256c4680c553621b1
grafix445 <65323921+grafix445@users.noreply.github.com>
2020-12-11 19:22:45 +0100

    0

--- bilder.py
+++ bilder.py
@@ -14,12 +14,17 @@ path_menu = '/home/pi/Desktop/LED_Frame/Menu/'
 
 #------------------------------------
 
+#gif_select = bilder_import.gif('Select.gif', path_menu)
+#gif_bye = bilder_import.gif('Bye.gif', path_menu)
+#gif_hello = bilder_import.gif('Hello.gif', path_menu)
+#gif_error = bilder_import.gif('Wrong.gif', path_menu)
+
 # LED strip configuration:
 LED_COUNT = 256        # Number of LED pixels.
-#LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
+LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
 LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+#LED_DMA = 10          # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 60  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
